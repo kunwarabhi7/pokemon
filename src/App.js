f9681c1b9182359b7494de2aca3a096f835a82b1
Abhishek <abhishekparmar162@gail.com>
2022-09-09 17:54:52 +0530

    added all files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Axios from 'axios';
 
 function App() {
+  const [name, setName] = useState('')
+  const [pokemonChoosen, setPokemonChoosen] = useState(false)
+  const [pokemon, setPokemon] = useState({})
+
+  const searchPokemon = () => {
+    Axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`).then(res => {
+      console.log(res)
+      setPokemon({
+
+        name: name, species: res.data.species.name, id: res.data.id,
+        height: res.data.height, weight: res.data.weight, OVR: res.data.base_experience,
+        image: res.data.sprites.front_default, type: res.data.types[0].type.name
+      })
+    })
+    setPokemonChoosen(true)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="TitleSection">
+        <h1>Pokemon</h1>
+        <input type="text" onChange={e => { setName(e.target.value) }} />
+        <button onClick={searchPokemon}>Search Pokemon</button>
+        {!pokemonChoosen ? <p>Choose a pokemon </p> :
+          <>
+            <h1>Name:{pokemon.name}</h1>
+            <h3>species:{pokemon.species} </h3>
+            <h2>Rank:{pokemon.id}</h2>
+            <h4>Height: {pokemon.height}</h4>
+            <h4>weight: {pokemon.weight}</h4>
+            <h4>OVR: {pokemon.OVR}</h4>
+            <h4>Type: {pokemon.type}</h4>
+            <img src={pokemon.image} />
+          </>
+        }
+
+      </div>
     </div>
+
   );
 }
 
